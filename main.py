efbbe77c8bcd4b8b6bd7c8c28dbe618561c9fc6c
whereismyguts <whereismyguts@gmail.com>
2018-02-13 15:53:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ from telegram.ext import Updater, CommandHandler
 
 
 def hello(bot, update):
+    print('hello')
     update.message.reply_text(
         'Hello {}'.format(update.message.from_user.first_name))
 
-
 def main():
+    print('start main')
     updater = Updater('412172586:AAFbIXMb0Gm8nci-cBNq7eS6CfGbNPetmU8')
 
     updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
     updater.start_polling()
     updater.idle()
+    print('idle')
 
 if __name__ == '__main__':  
     try:
         main()
     except KeyboardInterrupt:
+        print('nooo')
         exit()
