39c10569c3d6275d816afe760f906d019ea130c0
whereismyguts <whereismyguts@gmail.com>
2018-02-15 13:03:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
-from telegram.ext import Updater, CommandHandler
+# -*- coding: utf-8 -*-
+import telebot
+from telebot import types
+import os
 
+yarr = os.environ.get('YARR', None)
 
-def hello(bot, update):
-    print('hello')
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+bot = telebot.TeleBot(yarr)
 
-def main():
-    print('start main')
-    updater = Updater('412172586:AAFbIXMb0Gm8nci-cBNq7eS6CfGbNPetmU8')
+@bot.message_handler(commands=['fuck'])
+def fuck(message):
+    sent = bot.send_message(message.chat.id, "you've pressed 'fuck'")
+    bot.register_next_step_handler(sent, next_step)
 
-    updater.dispatcher.add_handler(CommandHandler('hello', hello))
+@bot.message_handler(commands=['test'])
+def test(message):
+    sent = bot.send_message(message.chat.id, "you've pressed 'test'")
 
-    updater.start_polling()
-    updater.idle()
-    print('idle')
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=5)
 
-if __name__ == '__main__':  
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('nooo')
-        exit()
+    keyboard.add(*[types.KeyboardButton(str(n+1)) for n in range(10)])
+
+    #keyboard.add(types.KeyboardButton('me'),types.KeyboardButton('so'),types.KeyboardButton('horny'))
+
+    sent_method = bot.send_message(message.chat.id, 'rate this shit', reply_markup=keyboard)
+
+    bot.register_next_step_handler(sent_method, choosen)
+
+def choosen(message):
+    sent_method = bot.send_message(message.chat.id, "thanks for rate (%r)"%("*"*int(message.text)), reply_markup=types.ReplyKeyboardRemove())
+   # bot.register_next_step_handler(sent_method, choosen)
+    
+def next_step(message):
+    bot.send_message(message.chat.id, "message sent from 'next_step' callback")
+
+def new_game():
+    # bot.add_message_handler({'fooock' : 'yoooou'})
+    bot.polling()
+
